9b06947e8dc9a52b5985f1b3b8d94875e6048839
DeavyJ <joelvangente--4@hotmail.com>
2019-02-25 17:44:37 +0000

    c

---
+++ PhoneLess_Project/SettingController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingController.swift
+//  PhoneLess_Project
+//
+//  Created by Joel Vangente on 25/02/2019.
+//  Copyright © 2019 Joel Vangente. All rights reserved.
+//
+
+import UIKit
+
+class SettingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
