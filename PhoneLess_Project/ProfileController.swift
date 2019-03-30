0dfad06a26f19da8df8139964362a06d8052b560
DeavyJ <joelvangente--4@hotmail.com>
2019-03-30 12:45:34 +0000

    Added info into plist

--- PhoneLess_Project/ProfileController.swift
+++ PhoneLess_Project/ProfileController.swift
@@ -11,19 +11,79 @@ import Firebase
 
 class ProfileController: UIViewController {
     
-   
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userSurname: UILabel!
+    @IBOutlet weak var userAddictionLevel: UILabel!
+    @IBOutlet weak var userActivityLevel: UILabel!
+    
+    @IBOutlet weak var pbest_Daily_Timeoff: UILabel!
+    @IBOutlet weak var pbest_Weekly_Timeoff: UILabel!
+    @IBOutlet weak var pbest_Daily_Steps: UILabel!
+    @IBOutlet weak var pbest_Weekly_Steps: UILabel!
+    
+    @IBOutlet weak var userImg: UIImageView!
+    
     var ref: DatabaseReference?
-    var databaseHandle: DatabaseHandle?
+    var handle: DatabaseHandle?
     
-    
+    var userID = Auth.auth().currentUser?.uid
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Database.database().reference()
         
-        
-
-        // Do any additional setup after loading the view.
     }
     
+    // Get info from DB
+    func get_data(){
+        // Get name info from DB
+        handle = ref?.child(userID!).child("Name").observe(.value, with: { (snpashot) in
+            let uName = snpashot.value as! String
+            if uName != ""{
+                self.userName.text = uName
+            }
+        })
+        // Get addiction info from DB
+        handle = ref?.child(userID!).child("Addiction Level").observe(.value, with: { (snpashot) in
+            let uAddiction = snpashot.value as! String
+            if uAddiction != ""{
+                self.userAddictionLevel.text = uAddiction
+            }
+        })
+        // Get activity info from DB
+        handle = ref?.child(userID!).child("Activity Level").observe(.value, with: { (snpashot) in
+            let uActivity = snpashot.value as! String
+            if uActivity != ""{
+                self.userActivityLevel.text = uActivity
+            }
+        })
+        // Get daily time off personal best info from DB
+        handle = ref?.child(userID!).child("Best Daily Time OFF").observe(.value, with: { (snpashot) in
+            let uBestDTimeOFF = snpashot.value as! String
+            if uBestDTimeOFF != ""{
+                self.pbest_Daily_Timeoff.text = uBestDTimeOFF
+            }
+        })
+        // Get weekly time off personal best info from DB
+        handle = ref?.child(userID!).child("Best Weekly Time OFF").observe(.value, with: { (snpashot) in
+            let uBestWTimeOFF = snpashot.value as! String
+            if uBestWTimeOFF != ""{
+                self.pbest_Weekly_Timeoff.text = uBestWTimeOFF
+            }
+        })
+        // Get daily steps personal best info from DB
+        handle = ref?.child(userID!).child("Best Daily Steps").observe(.value, with: { (snpashot) in
+            let uBestDSteps = snpashot.value as! String
+            if uBestDSteps != ""{
+                self.pbest_Daily_Steps.text = uBestDSteps
+            }
+        })
+        // Get weekly steps personal best info from DB
+        handle = ref?.child(userID!).child("Best Weekly Steps").observe(.value, with: { (snpashot) in
+            let uBestWSteps = snpashot.value as! String
+            if uBestWSteps != ""{
+                self.pbest_Weekly_Steps.text = uBestWSteps
+            }
+        })
+    }
 }
--- PhoneLess_Project/SettingController.swift
+++ PhoneLess_Project/SettingController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
-class SettingController: UIViewController {
+
+class SettingController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var viewSetGoals: UIView!
     @IBOutlet weak var uiviewViewGoals: UIView!
@@ -18,7 +20,30 @@ class SettingController: UIViewController {
     @IBOutlet weak var btnDisplayGoals: UIButton!
     @IBOutlet weak var btnChangeDetails: UIButton!
     @IBOutlet weak var btnUpdateImage: UIButton!
+    
+    @IBOutlet weak var txtDailyTimeOFF: UITextField!
+    @IBOutlet weak var txtWeeklyTimeOff: UITextField!
+    @IBOutlet weak var txtDailySteps: UITextField!
+    
+    @IBOutlet weak var txtActivityLevel: UITextField!
+    @IBOutlet weak var txtAddictionLevel: UITextField!
+    @IBOutlet weak var txtAddFriend: UITextField!
+    
+    
+    @IBOutlet weak var lblDaily_TimeOFF_Goal: UILabel!
+    @IBOutlet weak var lblWeekly_TimeOFF_Goal: UILabel!
+    @IBOutlet weak var lblDaily_Steps_Goal: UILabel!
+    
+    
+    @IBOutlet weak var btnSet_D_TimeOFF: UIButton!
+    @IBOutlet weak var btnSet_W_TimeOFF: UIButton!
+    @IBOutlet weak var btnSet_D_Steps: UIButton!
+    
     @IBOutlet weak var imgimageview: UIImageView!
+    
+    var ref:DatabaseReference!
+    var handle:DatabaseHandle?
+    var userID = Auth.auth().currentUser?.uid
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +51,57 @@ class SettingController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func set_up_daily_timeoff_Goal(_ sender: Any) {
+        if txtDailyTimeOFF.text != ""{
+            ref.child(userID!).child("Daily Time OFF Goal").setValue(txtDailyTimeOFF.text)
+            txtDailyTimeOFF.text = ""
+        }
+    }
+    @IBAction func set_up_weekly_timeoff_Goal(_ sender: Any) {
+        if txtWeeklyTimeOff.text != ""{
+            ref.child(userID!).child("Weekly Time OFF Goal").setValue(txtWeeklyTimeOff.text)
+            txtWeeklyTimeOff.text = ""
+        }
+    }
+    
+    @IBAction func set_up_daily_steps(_ sender: Any) {
+        if txtDailySteps.text != ""{
+            ref.child(userID!).child("Daily Steps Goal").setValue(txtDailySteps.text)
+            txtDailySteps.text = ""
+        }
+    }
+    
+    @IBAction func update_Activity_Level(_ sender: Any) {
+        if txtActivityLevel.text != ""{
+            ref.child(userID!).child("Activity Level").setValue(txtActivityLevel.text)
+            txtActivityLevel.text = ""
+        }
+    }
+    
+    @IBAction func update_Device_addiction(_ sender: Any) {
+        if txtAddictionLevel.text != ""{
+            ref.child(userID!).child("Addiction Level").setValue(txtAddictionLevel.text)
+            txtAddictionLevel.text = ""
+        }
+    }
+    
+    @IBAction func add_friend(_ sender: Any) {
+        let actual_User = Auth.auth().currentUser
+        if actual_User != nil{
+            try? Auth.auth().signOut()
+        }
+    }
+    
+    @IBAction func addImage(_ sender: Any) {
+        addImage()
+    }
+    
+    @IBAction func logOut(_ sender: Any) {
+        log_register()
+    }
+    
     @IBAction func setGoals(_ sender: Any) {
         if viewSetGoals.isHidden == true{
             viewSetGoals.isHidden = false
@@ -109,5 +185,70 @@ class SettingController: UIViewController {
         stckImageStack.isHidden = true
         btnUpdateImage.setImage(UIImage.init(named: "expamd"), for: .normal)
     }
+    
+    func addImage(){
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose source", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        picker.dismiss(animated: true, completion: nil)
+        var data = Data()
+        data = image.jpegData(compressionQuality: 0.5)!
+        
+        let imageRef = Storage.storage().reference().child(userID!).child("Profile Image")
 
+        _ = imageRef.putData(data, metadata: nil) { (metadata, error) in
+            guard metadata != nil else {
+                return
+            }
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func chechImages(){
+        let imagepath = userID
+        print (imagepath! + "/Images/Number1")
+        let image1 = Storage.storage().reference(withPath: imagepath! + "Profile Image")
+        
+        image1.getData(maxSize: 1 * 1024 * 1024) { data, error in
+            if error != nil {
+                // Add logo image if no image found
+                self.imgimageview.image = UIImage(named: "Logo")
+            } else {
+                // Data for "images"
+                self.imgimageview.image = UIImage(data: data!)
+                
+            }
+        }
+    }
+    
+    func log_register(){
+        let log_out = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        self.present(log_out, animated: true, completion: nil)
+    }
 }
